1f346ae472c4ad891d8c92097373096495054b2a
kklly17 <kellymontielcano17@hotmail.com>
2025-04-15 23:45:24 -0600

    menu, estructuras
    funcion borrador del menu, archivo de estructuras

--- Estructuras.h
+++ Estructuras.h
@@ -1,5 +1,67 @@
-#ifndef Estructuras_H_
-#define Estructuras_H_
+#ifndef ESTRUCTURAS_H_
+#define ESTRUCTURAS_H_
 
 
+struct direccion
+{
+	char calle[30], colonia[30], municipio[30], estado[30];
+	int numero;
+};
+
+struct fecha
+{
+	int dia, mes, anio;	
+};
+
+struct datosAlumnos
+{
+	int matricula;
+	int semestre;
+	int telefono[11];
+	char nombre[50];
+	char carrera[10];
+	char correo[50];
+	float promedio[10];
+	struct direccion dir;
+	struct fecha fecha_nacimiento;
+};
+
+struct datosProfes
+{
+	int ID_empleado;
+	int coordinacion;
+	int telefono[11];
+	int clave_materia[10];
+	char nombre[50];
+	char correo[50];
+	struct direccion dir;
+	struct fecha fecha_nacimiento;
+};
+
+struct datosMaterias
+{
+	int clave_materia;
+	int semestre;
+	char nombre[30];
+	char descripcion[60];
+};
+
+struct datosGrupos
+{
+	int clave_grupo;
+	int semestre;
+	int clave_materia[6];
+	int ID_empleado[6];
+    struct fecha f_actual;
+	int periodo;
+	int anio;
+};
+
+struct datosInscripcion
+{
+	int folio;
+	int matricula;
+	int clave_grupo;
+	struct fecha f_actual;
+};
 #endif
--- main.c
+++ main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void menu(int*);
 
 main()
 {
-  
+	FILE *archivoptr;
+	int opcion;
+	
+	menu(&opcion);
+	
+	while(opcion != 8)
+	{
+		switch(opcion)
+		{
+			case 1:
+				//if((archivoptr = fopen("")))
+				break;
+		}
+		
+		menu(&opcion);
+	}
+
+}
+
+void menu(int *opcion)
+{
+	do
+	{
+		printf("\n\t\t\tMENU PRINCIPAL");
+		printf("\n\t============================================");
+		printf("\n\t\t[1] Alumnos");
+		printf("\n\t\t[2] Profesor");
+		printf("\n\t\t[3] Materias");
+		printf("\n\t\t[4] Grupo");
+		printf("\n\t\t[5] Inscripcion");
+		printf("\n\t\t[6] Actualizar promedio");
+		printf("\n\t\t[7] Reportes");
+		printf("\n\t\t[8] Salir");
+		printf("\n\t   Elija una opcion: ");
+		scanf("%d",opcion);
+		printf("\t---------------------------------------------");
+		if(*opcion < 1 || *opcion > 8)
+		{
+			system("cls");
+			printf("\n\t\tOpcion invalida. Intente de nuevo\n");
+		}
+	}while(*opcion < 1 || *opcion > 8);	
 }
